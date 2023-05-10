d51208ffb09c37e4832be7112ce7f09bbe67e5c3
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-05-10 12:55:03 -0600

    Adds modified index.js package.json and package-lock.json

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {
+    return string.toUpperCase(); 
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+   console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase())
+}
+
+/*function sayHiToHeadphonedRoommate(string) {
+    return string.toLowerCase ? "I can\'t hear you!" : "YES"
+}*/
+
+/*function sayHiToHeadphonedRoommate(string) {
+    returnif (string === "string.toLowerCase()") {
+         'I can\'t hear you!';
+ }}*/
+
+function sayHiToHeadphonedRoommate(string) {
+     
+let greeting;
+switch (string) {
+  case string.toUpperCase() :
+    greeting = "YES INDEED!";
+    break;
+  case string.toLowerCase():
+    greeting = "I can't hear you!";
+    break;
+  case "Let's have dinner together!":
+    greeting = "I would love to!";
+    break;
+} return (greeting);}
+
